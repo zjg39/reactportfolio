595be8626df1ed92b14dfb32c4347d492f6071b7
Zac Goad <zjgoad@gmail.com>
2021-11-22 22:48:05 -0500

    contact form and email validation

---
+++ src/components/contact.js
@@ -0,0 +1,105 @@
+
+import React, { useState } from "react";
+import { validation } from "../utils/helpers";
+
+
+function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const correctInput = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+        if (inputType === "name") {
+            setName(inputValue);
+        } else if (inputType === "email") {
+            setEmail(inputValue)
+        } else {
+            setMessage(inputValue);
+        }
+    }
+    const submission = (e) => {
+        e.preventDefault();
+        if (!validation(email)) {
+            setError("Please enter a valid email address.")
+            return;
+        } else if (!name) {
+            setError("Don\'t be a stranger!")
+            return;
+        } else if (!message) {
+            setError("Please leave a message.")
+            return;
+        }
+        setEmail("");
+        setName("");
+        setMessage("");
+        setError("");
+    };
+    return (
+        <div className="container">
+          <h1 style={{ color: "lightcyan" }}>Contact</h1>
+          <form>
+            <div className="form-row">
+              <div className="form-group col-md-6">
+                <label htmlFor="name" style={{ color: "lightcyan" }}>
+                  Your Name:
+                </label>
+                <input
+                  value={name}
+                  name="name"
+                  onChange={correctInput}
+                  type="text"
+                  className="form-control"
+                  placeholder="What's your name?"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="email" style={{ color: "lightcyan" }}>
+                  Email Address:
+                </label>
+                <input
+                  value={email}
+                  name="email"
+                  onChange={correctInput}
+                  type="email"
+                  className="form-control"
+                  placeholder="What is your email address?"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <label htmlFor="message" style={{ color: "lightcyan" }}>
+                  Message:
+                </label>
+                <textarea
+                  value={message}
+                  name="message"
+                  onChange={correctInput}
+                  type="text"
+                  className="form-control"
+                  placeholder="Leave a message!"
+                />
+              </div>
+              {error && (
+                <div>
+                  <p className="error-text" style={{ color: "lightcyan" }}>
+                    {error}
+                  </p>
+                </div>
+              )}
+              <div class="mt-5 mb-5">
+                <button
+                  className="btn btn-dark"
+                  type="button"
+                  onClick={submission}
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      );
+}
+export default Contact;
---
+++ src/utils/helpers.js
@@ -0,0 +1,5 @@
+export function validation(email) {
+    const  regex =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    return regex.test(String(email).toLowerCase());
+  }
